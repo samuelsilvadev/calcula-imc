8b91e696981eb5edb4aa4f1105341b32462139ae
samuel <samuel@sotechsaude.com.br>
2016-10-11 12:42:33 -0300

    criando objeto paciente

--- js/calcula-imc.js
+++ js/calcula-imc.js
@@ -4,12 +4,19 @@ var imc;
 
 for (var i = 0; trsPaciente.length >= i; i++) {
 
-	var tdPaciente 	= trsPaciente[i].getElementsByClassName("nomePaciente")[0].textContent;
-	var tdAltura 	= trsPaciente[i].getElementsByClassName("altura")[0].textContent;
-	var tdPeso 		= trsPaciente[i].getElementsByClassName("peso")[0].textContent;
+	var tdPaciente 	= trsPaciente[i].getElementsByClassName("nomePaciente")[0];
+	var tdAltura 	= trsPaciente[i].getElementsByClassName("altura")[0];
+	var tdPeso 		= trsPaciente[i].getElementsByClassName("peso")[0];
 
-	if(tdAltura > 0){
-		imc =  tdPeso / (tdAltura * tdAltura);
+	var paciente = {
+		nome : tdPaciente.textContent,
+		altura : tdAltura.textContent,
+		peso : tdPeso.textContent
+	}
+
+
+	if(paciente.altura > 0){
+		imc =  paciente.peso / (paciente.altura * paciente.altura);
 		trsPaciente[i].getElementsByClassName("imc")[0].textContent = imc;
 	}else{
 		trsPaciente[i].getElementsByClassName("imc")[0].textContent = "Altura inválida";
