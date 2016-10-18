f51f1544f7430ece36e78301924132cdc20fb9f8
samuel <samuelsilvawb@gmail.com>
2016-10-18 20:28:18 -0300

    starting codes to take fields values

---
+++ js/adiciona.js
@@ -0,0 +1,17 @@
+var table = document.querySelector("table");
+
+console.log(table);
+
+/*var nome = document.querySelector("#novo-nome");
+var altura = document.querySelector("#nova-altura");
+var peso = document.querySelector("#novo-peso");
+
+var novoPaciente = 	"<tr class='paciente'>"+
+						"<td class='nomePaciente'>"+nome+"</td>"+
+						"<td class='altura'>"+altura+"</td>"+
+						"<td class='peso'>"+peso+"</td>"+
+						"<td class='imc'></td>"+
+					"</tr>";
+
+
+*/
